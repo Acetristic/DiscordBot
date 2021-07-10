632515da076dc530f32fdf1227557e711f4a81d6
Acetristic <87184644+Acetristic@users.noreply.github.com>
2021-07-10 09:21:23 -0400

    Create warnings.js

---
+++ commands/moderation/warnings.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { Color } = require("../../config.js");
+
+module.exports = {
+  name: "warnings",
+  aliases: ["warning"],
+  description: "Shows User Warnings!",
+  usage: "Warnings <Mention User>",
+  run: async (client, message, args) => {
+    //Start
+
+    let Member =
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]);
+
+    if (!Member) return message.channel.send("Pls mention a user you want to view warnings of.");
+
+    let Warnings = client.db.get(
+      `Warnings_${message.guild.id}_${Member.user.id}`
+    );
+
+    let embed = new MessageEmbed()
+      .setColor(Color)
+      .setTitle(`Member Warnings!`)
+      .setDescription(`${Member.user.username} Has ${Warnings || "0"} Warnings!`)
+      .setFooter(`Requested by ${message.author.username}`)
+      .setTimestamp();
+
+    message.channel.send(embed);
+
+    //End
+  }
+};
